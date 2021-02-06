fa20b94c92d485068a93dfcffb0dd021d97c7f45
Anup Yadav <anup87yadav@gmail.com>
2021-02-07 05:27:58 +0530

    updated code

--- test/test_api.py
+++ test/test_api.py
@@ -1,36 +1,37 @@
 import requests
 import logging
-from ratelimit import limits, RateLimitException
+from ratelimit import limits, RateLimitException, rate_limited
 import time
 
 
 class TestAPI:
 
-    ONE_MINUTE = 60
-    ''' Verify the response status code'''
-    @limits(calls=5, period=ONE_MINUTE)
-    def test_verifyStatusCode(self):
+    ONE_DAY = 3600 * 24
 
-        try:
-            q = {"function": "INCOME_STATEMENT", "symbol": "IBM", "apikey": "apikey"}
-            resp = requests.get("https://www.alphavantage.co/query", params=q)
-            logging.info(resp)
-            if resp.status_code == 429:
-                raise RateLimitException("too many calls", 60)
-            assert resp.status_code == 200, "not valid code"
-        except Exception as e:
-            print(e)
+    ''' Verify the response status code'''
+    @rate_limited(calls=500, period=ONE_DAY)
+    def test_verifyStatusCode(self):
+        @rate_limited(calls=5, period=60)
+        def test_apicalltest():
+            try:
+                q = {"function": "INCOME_STATEMENT", "symbol": "IBM", "apikey": "apikey"}
+                resp = requests.get("https://www.alphavantage.co/query", params=q)
+                logging.info(resp)
+                if resp.status_code == 429:
+                    raise RateLimitException("too many calls", 60)
+                assert resp.status_code == 200, "not valid code"
+            except Exception as e:
+                print(e)
 
     '''Verify the Response Content-Type'''
-    @limits(calls=5, period=ONE_MINUTE)
+    @limits(calls=5, period=60)
     def test_verifyheaders(self):
-
         q = {"function": "INCOME_STATEMENT", "symbol": "IBM", "apikey": "apikey"}
         resp = requests.get("https://www.alphavantage.co/query", params=q)
         assert resp.headers.__contains__("Content-Type")
 
     '''Verify the URL'''
-    @limits(calls=5, period=ONE_MINUTE)
+    @limits(calls=5, period=60)
     def test_verifyurl(self):
 
         q = {"function": "INCOME_STATEMENT", "symbol": "IBM", "apikey": "apikey"}
@@ -38,14 +39,14 @@ class TestAPI:
         print(resp.url)
 
     '''Verify Response Time'''
-    @limits(calls=5, period=ONE_MINUTE)
+    @limits(calls=5, period=60)
     def test_verifyresponsetime(self):
         q = {"function": "INCOME_STATEMENT", "symbol": "IBM", "apikey": "apikey"}
         resp = requests.get("https://www.alphavantage.co/query", params=q)
         assert resp.elapsed.total_seconds() < 200, "not valid time limit"
 
     '''Verify the symbol value in response body'''
-    @limits(calls=5, period=ONE_MINUTE)
+    @limits(calls=5, period=60)
     def test_verifysymbolvalue(self):
         q = {"function": "INCOME_STATEMENT", "symbol": "IBM", "apikey": "apikey"}
         resp = requests.get("https://www.alphavantage.co/query", params=q)
@@ -56,7 +57,7 @@ class TestAPI:
         assert (body["symbol"]) == "IBM", "Not Found"
 
     '''Verify the reported Currency type in response body'''
-    @limits(calls=5, period=ONE_MINUTE)
+    @limits(calls=5, period=60)
     def test_verifyreportedcurrency(self):
         q = {"function": "INCOME_STATEMENT", "symbol": "IBM", "apikey": "apikey"}
         resp = requests.get("https://www.alphavantage.co/query", params=q)
